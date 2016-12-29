97e4a6c13d839be210dc2dcf7ddb1ce394a9b99c
Unaivero <unaivero@gmail.com>
2016-12-29 18:50:19 +0100

    Update feedsterngPostController.js

--- feedsterngPostController.js
+++ feedsterngPostController.js
@@ -3,7 +3,7 @@ app.controller('PostController', ['$scope', function($scope) {
     {
       author: {
         name: 'Calvin Broadus, Jr.',
-        avatar: 'https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png'
+        avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/9e/Creative-Tail-People-gentleman.svg/128px-Creative-Tail-People-gentleman.svg.png'
       },
       comment: {
         img: 'https://cdn.pixabay.com/photo/2016/01/19/17/54/dog-1149964_960_720.jpg',
@@ -15,7 +15,7 @@ app.controller('PostController', ['$scope', function($scope) {
     {
       author: {
         name: 'Matthew Healy',
-        avatar: 'https://cdn.pixabay.com/photo/2016/04/01/10/11/avatar-1299805_960_720.png'
+        avatar: 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/63/Creative-Tail-People-businness-man.svg/128px-Creative-Tail-People-businness-man.svg.png'
       },
       comment: {
         text: 'I used to have a recurring dream when I was younger.'
